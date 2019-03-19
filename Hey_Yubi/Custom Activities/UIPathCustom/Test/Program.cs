e76aff116b79f731788ce6a485bca56193e847f5
Tony-Chau <tony_chau23@hotmail.com>
2019-03-19 19:20:04 +1000

    Included Nuget Package made before. Text to Speech. Speech to Text will be deleted soon.

---
+++ Hey_Yubi/Custom Activities/UIPathCustom/Test/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.AudioFormat;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            SpeechRecognizer recon = new SpeechRecognizer();
+            Choices choice = new Choices();
+            choice.Add(new string[] { "red", "green", "blue" });
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(choice);
+
+            Grammar g = new Grammar(gb);
+            gb.Culture = Thread.CurrentThread.CurrentCulture;
+            recon.LoadGrammar(g);
+            recon.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecongnized);
+            Console.ReadKey();*/
+
+            SpeechRecognizer recon = new SpeechRecognizer();
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Volume = 100;
+            synth.Rate = 0;
+            synth.Speak("Say Something now");
+
+            DictationGrammar Grammar = new DictationGrammar();
+            SpeechRecognitionEngine SpeechEngine = new SpeechRecognitionEngine();
+            SpeechEngine.LoadGrammar(Grammar);
+            recon.LoadGrammar(Grammar);
+            recon.SpeechRecognized += EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecongnized);
+            /*
+            SpeechEngine.SetInputToDefaultAudioDevice();
+            var sp = SpeechEngine.Recognize();
+            Console.WriteLine(sp.Text);*/
+
+            Console.ReadKey();
+            
+        }
+
+        private static EventHandler<T> EventHandler<T>(Action<object, T> sre_SpeechRecongnized)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void sre_SpeechRecongnized(object arg1, SpeechRecognizedEventArgs arg2)
+        {
+            Console.WriteLine(arg2.ToString());
+        }
+
+
+    }
+}
---
+++ Hey_Yubi/Custom Activities/UIPathCustom/UIPathCustom/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace UIPathCustom
+{
+    public class TextToSpeech : CodeActivity
+    {
+
+        [Category("Input")]
+        public InArgument<int> Volume { get; set; }
+
+        [Category("Input")]
+        public InArgument<int> Rate { get; set; }
+
+        [Category("Input")]
+        public InArgument<string> Message { get; set; }
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Volume = Volume.Get(context);
+            synth.Rate = Rate.Get(context);
+            synth.Speak(Message.Get(context));
+        }
+    }
+    public class SpeechToText : CodeActivity
+    {
+        [Category("Output")]
+        public OutArgument<string> Message { get; set; }
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            DictationGrammar Grammar = new DictationGrammar();
+            SpeechRecognitionEngine SpeechEngine = new SpeechRecognitionEngine();
+            SpeechEngine.LoadGrammar(Grammar);
+            SpeechEngine.SetInputToDefaultAudioDevice();
+            Message.Set(context, SpeechEngine.Recognize().Text);
+
+        }
+    }
+}
