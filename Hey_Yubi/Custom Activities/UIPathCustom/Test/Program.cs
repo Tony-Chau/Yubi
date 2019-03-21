972fb2164c4a1d1a5468ef93b062df9c8d0791d6
Tony-Chau <tonychau923@gmail.com>
2019-03-22 09:41:31 +1000

    Added more nuget packages and tested Youtube API on seperate solution and functioning well. Will be included within the custom soon

--- Hey_Yubi/Custom Activities/UIPathCustom/Test/Program.cs
+++ Hey_Yubi/Custom Activities/UIPathCustom/Test/Program.cs
@@ -10,6 +10,10 @@ using System.Threading;
 using System.Media;
 using System.IO;
 
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
 /// <summary>
 /// The Test file is there to help me what program is needed 
 /// </summary>
@@ -19,54 +23,9 @@ namespace Test
     class Program
     {
         static void Main(string[] args)
-        {/*
-            SpeechRecognizer recon = new SpeechRecognizer();
-            Choices choice = new Choices();
-            choice.Add(new string[] { "red", "green", "blue" });
-            GrammarBuilder gb = new GrammarBuilder();
-            gb.Append(choice);
-
-            Grammar g = new Grammar(gb);
-            gb.Culture = Thread.CurrentThread.CurrentCulture;
-            recon.LoadGrammar(g);
-            recon.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecongnized);
-            Console.ReadKey();*/
-         /*
-         SpeechRecognizer recon = new SpeechRecognizer();
-         SpeechSynthesizer synth = new SpeechSynthesizer();
-         synth.Volume = 100;
-         synth.Rate = 0;
-         synth.Speak("Say Something now");
-
-         DictationGrammar Grammar = new DictationGrammar();
-         SpeechRecognitionEngine SpeechEngine = new SpeechRecognitionEngine();
-         SpeechEngine.LoadGrammar(Grammar);
-         recon.LoadGrammar(Grammar);
-         recon.SpeechRecognized += EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecongnized);
-         /*
-         SpeechEngine.SetInputToDefaultAudioDevice();
-         var sp = SpeechEngine.Recognize();
-         Console.WriteLine(sp.Text);*/
-            string currentdirectory = Directory.GetCurrentDirectory();
-            int endindex = currentdirectory.Length - 10;
-            currentdirectory = currentdirectory.Substring(0, endindex);
-            currentdirectory += "\\wav\\beep-07.wav";
-            SoundPlayer simpleSound = new SoundPlayer(currentdirectory);
-            simpleSound.Play();
-            Console.ReadKey();
-
-        }
-
-        private static EventHandler<T> EventHandler<T>(Action<object, T> sre_SpeechRecongnized)
         {
-            throw new NotImplementedException();
+           
         }
-
-        private static void sre_SpeechRecongnized(object arg1, SpeechRecognizedEventArgs arg2)
-        {
-            Console.WriteLine(arg2.ToString());
-        }
-
 
     }
 }
---
+++ Hey_Yubi/Custom Activities/UIPathCustom/Youtube/Search.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace Youtube
+{
+    //code demo from https://developers.google.com/youtube/v3/code_samples/dotnet
+    internal class Search
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YouTube Data API: Search");
+            Console.WriteLine("========================");
+            try
+            {
+                new Search().Run().Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private async Task Run()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyBnZWoU3n6sDYKz7UzB-5JuzUNJnlVLw7E",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = "Google"; // Your query. Literally your search
+            searchListRequest.MaxResults = 50;
+
+            //Call the search.list method to retrieve results matching the specific query term
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            List<string> vid = new List<string>();
+
+            foreach (var SearchResult in searchListResponse.Items)
+            {
+                if (SearchResult.Id.Kind == "youtube#video")
+                {
+                    vid.Add(string.Format("{0} {1} ", SearchResult.Snippet.Title, SearchResult.Id.VideoId));
+
+                }
+            }
+
+            Console.WriteLine(string.Format("Videos:\n{0}\n", string.Join("\n", vid)));
+        }
+    }
+}
