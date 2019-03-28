4c47251acb59360be689db1a292666f815865532
Tony-Chau <tonychau923@gmail.com>
2019-03-28 16:30:07 +1000

    Made a new project folder to test our vb form, but youtube is no longer supported on form

---
+++ Hey_Yubi/Custom Activities/UIPathCustom/WindowsApp1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub AxShockwaveFlash1_Enter(sender As Object, e As EventArgs) Handles AxShockwaveFlash1.Enter
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AxShockwaveFlash1.MovieData = "https://www.youtube.com/v/crwL4gQJHn0"
+    End Sub
+End Class
