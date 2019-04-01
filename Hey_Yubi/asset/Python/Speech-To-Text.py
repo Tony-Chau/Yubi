15ab71889ac57a9224d764f94751a97420025964
Tony-Chau <tonychau923@gmail.com>
2019-04-01 10:58:56 +1000

    updated the python code. Will test more on timeout

--- Hey_Yubi/asset/Python/Speech-To-Text.py
+++ Hey_Yubi/asset/Python/Speech-To-Text.py
@@ -1,23 +1,11 @@
 import speech_recognition as sr
 import pyaudio
-from threading import Thread, Event
-import time
 
-stop_event = Event()
-word = ""
-def speechaction():
+def SpeechToText(sec=5):
     r = sr.Recognizer()
     with sr.Microphone() as source:
         audio = r.listen(source)
     try:
-        word = r.recognize_google(audio)
+        return r.recognize_google(audio)
     except:
-        word = ""
-
-def SpeechToText(sec=5):
-    action_thread = Thread(target=speechaction)
-    action_thread.start()
-    action_thread.join(timeout=sec)
-
-    stop_event.set()
-    return word
+        return ""
--- Hey_Yubi/asset/Python/testing prototype/Speech.py
+++ Hey_Yubi/asset/Python/testing prototype/Speech.py
@@ -1,13 +1,28 @@
 import speech_recognition as sr
 import pyaudio
 
-r = sr.Recognizer()
+import speech_recognition as sr
+import pyaudio
+from threading import Thread, Event
+import time
 
-with sr.Microphone() as source:
-    print('Speak Anything: ')
-    audio = r.listen(source)
-try:
-    text = r.recognize_google(audio)
-    print('You said: {}'.format(text))
-except:
-    print('Sorry, could not recognize your voice')
+stop_event = Event()
+word = ""
+def speechaction():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+    try:
+        word = r.recognize_google(audio)
+    except:
+        word = ""
+
+def SpeechToText(sec=5):
+    action_thread = Thread(target=speechaction)
+    action_thread.start()
+    action_thread.join(timeout=sec)
+
+    stop_event.set()
+    return word
+
+print(SpeechToText(5))
